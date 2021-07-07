0cf6edd6353fe0361f0ffdaadd749b2df851ad71
mehdiouaz <85231294+mehdiouaz@users.noreply.github.com>
2021-07-07 19:12:36 +0100

    Add files via upload

---
+++ 3rd algo checkpoint/js.js
@@ -0,0 +1,17 @@
+//Write a procedure, called dot_product which calculates in the variable ps,
+// the dot(scalar) product of v1 and v2 (v1 and v2 are vectors of IR)
+var a = [2,7]
+var b = [8,5]
+var somm = a*b
+a*b = 2(8) + 7(5);
+    = 16+35= 51
+
+
+//Write an algorithm which determines, for n pairs of given vectors, whether two vectors of given IR are orthogonal, by calling the procedure defined in the previous question. 
+//The dot product of two orthogonal vectors is zero
+
+var a = [3,5]
+var b = [5,-3]
+    a*b=0
+    a*b=3*(5) * 5*(-3)
+       = 15*(-15)=0
